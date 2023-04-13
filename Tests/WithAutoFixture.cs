e994900b009137a1916c67679acd9fecf9a2c6b5
imp111 <alexsandartenev@gmail.com>
2023-04-13 11:28:18 +0300

    opentechnology and addnewtodoitem methods added

--- Tests/WithAutoFixture.cs
+++ Tests/WithAutoFixture.cs
@@ -1,12 +1,7 @@
 ﻿using AutoFixture;
 using AutoFixture.Xunit2;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 namespace Tests
--- ToDoTests/UnitTest1.cs
+++ ToDoTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace ToDoTests
 {
@@ -10,11 +12,13 @@ namespace ToDoTests
         private const string URL = "https://todomvc.com/";
         private readonly IWebDriver _driver;
         private readonly WebDriverWait _wait;
+        private readonly Actions _action;
 
         public UnitTest1()
         {
             _driver = new ChromeDriver();
             _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(WAIT_TIME));
+            _action = new Actions(_driver);
         }
 
         public void Dispose()
@@ -23,10 +27,31 @@ namespace ToDoTests
             _driver.Dispose();
         }
 
+        private IWebElement WaitUntilElementIsFound(By locator)
+        {
+            return _wait.Until(ExpectedConditions.ElementExists(locator));
+        }
+
+        private void OpenTechnology(string name)
+        {
+            var technologyLink = WaitUntilElementIsFound(By.LinkText(name));
+            technologyLink.Click();
+        }
+
+        private void AddNewTodoItem(string input)
+        {
+            var todoInputField = WaitUntilElementIsFound(By.XPath("//input[@placeholder=\"What needs to be done?\"]"));
+            todoInputField.SendKeys(input);
+
+            _action.Click(todoInputField).SendKeys(Keys.Enter).Perform();
+        }
+
         [Fact]
         public void VerifyTodoIsCreated()
         {
             _driver.Navigate().GoToUrl(URL);
+            OpenTechnology("React");
+            AddNewTodoItem("Breakfast");
         }
     }
 }
